5209c9d721c275cfaff8ceeb55d1042f4c8f558f
deen13 <dierkes@smartsquare.de>
2021-03-16 11:33:53 +0100

    Post name and ip

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const {context} = require("@actions/github")
 const {getOctokit} = require("@actions/github")
 const k8s = require('@kubernetes/client-node');
 
+function createComment(response) {
+    const externalIp = response.body.status.loadBalancer.ingress[0].ip
+    const name = response.body.metadata.name
+
+    return `${name} is now available at ${externalIp} :sunglasses:`
+}
+
 try {
     if (context.payload.pull_request == null) {
         core.setFailed("No pull request found.")
@@ -16,12 +23,8 @@ try {
     const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
     k8sApi.readNamespacedServiceStatus(core.getInput("loadbalancer"), core.getInput("namespace"))
-        .then((serviceStatus) => serviceStatus.body.status.loadBalancer.ingress[0].ip)
-        .then((ip) => ({
-            ...context.repo,
-            issue_number: context.payload.pull_request.number,
-            body: `Test: ${ip}`
-        }))
+        .then((serviceStatus) => createComment(serviceStatus))
+        .then((body) => ({...context.repo, issue_number: context.payload.pull_request.number, body: body}))
         .then((payload) => octokit.issues.createComment(payload))
         .catch((e) => console.log(e))
 } catch (error) {
