9d96247c882f8c483c88334d5d8f8e113b604f48
deen13 <dierkes@smartsquare.de>
2021-03-16 10:20:52 +0100

    yarn lint --fix

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 const core = require("@actions/core")
-const {getInput, setFailed} = require("@actions/core")
-const {context} = require("@actions/github")
+const { context } = require("@actions/github")
 
 try {
-    const github_token = core.getInput('GITHUB_TOKEN');
+  const github_token = core.getInput("GITHUB_TOKEN")
 
-    if (context.payload.pull_request == null) {
-        core.setFailed('No pull request found.');
-        return;
-    }
+  if (context.payload.pull_request == null) {
+    core.setFailed("No pull request found.")
+    return
+  }
 
-    const pull_request_number = context.payload.pull_request.number;
+  const pull_request_number = context.payload.pull_request.number
 
-    const octokit = new github.GitHub(github_token);
+  const octokit = new github.GitHub(github_token)
 
-    octokit.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request_number,
-        body: "Testnachricht"
-    });
+  octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request_number,
+    body: "Testnachricht",
+  })
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message)
 }
