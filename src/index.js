ecd3d8153f1a69adb71f11e9047cbc8a64ac1bf6
deen13 <dierkes@smartsquare.de>
2021-03-16 10:55:00 +0100

    Try to fetch service status

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const core = require("@actions/core")
 const {context} = require("@actions/github")
 const {getOctokit} = require("@actions/github")
+const k8s = require('@kubernetes/client-node');
 
 try {
+
     const github_token = core.getInput("token")
 
     if (context.payload.pull_request == null) {
         core.setFailed("No pull request found.")
         return
     }
+
+    const kc = new k8s.KubeConfig();
+    kc.loadFromDefault();
+    const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+    k8sApi.readNamespacedServiceStatus(core.getInput("loadbalancer"), core.getInput("namespace"))
+        .then((serviceStatus) => console.log(serviceStatus))
+        .catch(() => console.log("Failed to fetch service status."))
 
     const octokit = getOctokit(github_token)
 
