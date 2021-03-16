430fbd09d1b0f65a760b8fd053d46a2002683fe7
deen13 <dierkes@smartsquare.de>
2021-03-16 10:37:33 +0100

    Use getOctokit(...)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 const core = require("@actions/core")
-const { context } = require("@actions/github")
+const {context} = require("@actions/github")
+const {getOctokit} = require("@actions/github")
 
 try {
-  const github_token = core.getInput("GITHUB_TOKEN")
+    const github_token = core.getInput("GITHUB_TOKEN")
 
-  if (context.payload.pull_request == null) {
-    core.setFailed("No pull request found.")
-    return
-  }
+    if (context.payload.pull_request == null) {
+        core.setFailed("No pull request found.")
+        return
+    }
 
-  const pull_request_number = context.payload.pull_request.number
+    const octokit = getOctokit(github_token)
 
-  const octokit = new github.GitHub(github_token)
-
-  octokit.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request_number,
-    body: "Testnachricht",
-  })
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: context.payload.pull_request.number,
+        body: "Test"
+    });
 } catch (error) {
-  core.setFailed(error.message)
+    core.setFailed(error.message)
 }
