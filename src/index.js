bb34ad922bdfb560416c0e815f2ad297d40cbc79
deen13 <dierkes@smartsquare.de>
2021-03-16 10:12:18 +0100

    Execute action on pr

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const core = require("@actions/core")
-// const {getInput, setFailed} = require("@actions/core")
-// const {context} = require("@actions/github")
+const {getInput, setFailed} = require("@actions/core")
+const {context} = require("@actions/github")
 
-async function main() {
-    core.info("Fetching packages")
+try {
+    const github_token = core.getInput('GITHUB_TOKEN');
+
+    if (context.payload.pull_request == null) {
+        core.setFailed('No pull request found.');
+        return;
+    }
+
+    const pull_request_number = context.payload.pull_request.number;
+
+    const octokit = new github.GitHub(github_token);
+
+    octokit.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request_number,
+        body: "Testnachricht"
+    });
+} catch (error) {
+    core.setFailed(error.message);
 }
-
-main()
-    .then()
-    .catch((error) => setFailed(error.message))
