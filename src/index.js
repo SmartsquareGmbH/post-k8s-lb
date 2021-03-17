1630961f4c573e1e502b2f5103b9a5e99611c21f
deen13 <dierkes@smartsquare.de>
2021-03-17 11:15:58 +0100

    Fix linting

--- src/index.js
+++ src/index.js
@@ -1,54 +1,57 @@
-const {isLoadBalancer, hasExternalAddress, createComment} = require("./kubernetes.js")
+const { isLoadBalancer, hasExternalAddress, createComment } = require("./kubernetes.js")
 
 const promiseRetry = require("promise-retry")
 const k8s = require("@kubernetes/client-node")
 
 const core = require("@actions/core")
-const {context} = require("@actions/github")
-const {getOctokit} = require("@actions/github")
+const { context } = require("@actions/github")
+const { getOctokit } = require("@actions/github")
 
 async function fetchServiceStatus() {
-    const kc = new k8s.KubeConfig()
-    kc.loadFromDefault()
-    const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
+  const kc = new k8s.KubeConfig()
+  kc.loadFromDefault()
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
 
-    const response = await k8sApi.readNamespacedServiceStatus(core.getInput("loadbalancer"), core.getInput("namespace"))
+  const response = await k8sApi.readNamespacedServiceStatus(core.getInput("loadbalancer"), core.getInput("namespace"))
 
-    if (!isLoadBalancer(response.body)) {
-        throw Error("Given service is not a load balancer.")
-    }
+  if (!isLoadBalancer(response.body)) {
+    throw Error("Given service is not a load balancer.")
+  }
 
-    if (!hasExternalAddress(response.body)) {
-        throw Error("Given load balancer has no external ip assigned yet.")
-    }
+  if (!hasExternalAddress(response.body)) {
+    throw Error("Given load balancer has no external ip assigned yet.")
+  }
 
-    return response.body
+  return response.body
 }
 
 const run = async () => {
-    if (context.payload.pull_request == null) {
-        throw Error("No pull request found.")
+  if (context.payload.pull_request == null) {
+    throw Error("No pull request found.")
+  }
+
+  const service = await promiseRetry(function (retry, number) {
+    if (number > 1) {
+      console.log(`Retry Attempt: ${number}.`)
     }
 
-    const service = await promiseRetry(function (retry, number) {
-        if (number > 1) {
-            console.log(`Retry Attempt: ${number}.`)
-        }
+    return fetchServiceStatus().catch(retry)
+  })
 
-        return fetchServiceStatus().catch(retry)
-    })
+  const body = createComment(service)
 
-    const body = createComment(service)
+  const octokit = getOctokit(core.getInput("token"))
 
-    const octokit = getOctokit(core.getInput("token"))
+  const { data: comments } = octokit.issues.listComments({
+    ...context.repo,
+    number: context.payload.pull_request.number,
+  })
 
-    const {data: comments} = octokit.issues.listComments({...context.repo, number: context.payload.pull_request.number})
+  console.log(comments)
 
-    console.log(comments)
-
-    octokit.issues.createComment({...context.repo, issue_number: context.payload.pull_request.number, body: body})
+  octokit.issues.createComment({ ...context.repo, issue_number: context.payload.pull_request.number, body: body })
 }
 
 run()
-    .then()
-    .catch((error) => core.setFailed(error.message))
+  .then()
+  .catch((error) => core.setFailed(error.message))
